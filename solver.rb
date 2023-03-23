9a41becc3d1354b6c03826e00d1732d4bdaeaf58
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-23 12:19:00 +0300

    Fix linters errors

--- solver.rb
+++ solver.rb
@@ -1,25 +1,27 @@
-class Solver 
-    def factorial(n)
-        return 1 if n == 0
-        raise ArgumentError if n < 0
-        return n * factorial(n-1)
-    end
+class Solver
+  def factorial(num)
+    return 1 if num.zero?
+    raise ArgumentError if num.negative?
 
-    def reverse(word)
-        i = 0
-        j = word.length - 1
-        while i < j
-            word[i], word[j] = word[j], word[i]
-            i += 1
-            j -= 1
-        end
-        word
-    end
+    num * factorial(num - 1)
+  end
 
-    def fizzbuzz(n)
-        return "fizzbuzz" if(n%3==0 && n%5==0)
-        return "fizz" if(n%3==0)
-        return "buzz" if(n%5==0)
-        return "#{n}"
+  def reverse(word)
+    i = 0
+    j = word.length - 1
+    while i < j
+      word[i], word[j] = word[j], word[i]
+      i += 1
+      j -= 1
     end
+    word
+  end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,22 +1,22 @@
-require_relative '../solver.rb'
+require_relative '../solver'
 
 describe Solver do
-    before(:each) do
-        @solver = Solver.new
-    end
+  before(:each) do
+    @solver = Solver.new
+  end
 
-    describe "#fizzbuzz" do
-        it("should return fizz if n divisible by 3") do
-            expect(@solver.fizzbuzz(3)).to eq "fizz"
-        end
-        it("should return buzz if n divisible by 5") do
-            expect(@solver.fizzbuzz(5)).to eq "buzz"
-        end
-        it("should return fizz if n divisible by 3") do
-            expect(@solver.fizzbuzz(15)).to eq "fizzbuzz"
-        end
-        it("should return n as a string if n is not divisible by 3 or 5") do
-            expect(@solver.fizzbuzz(7)).to eq "7"
-        end
+  describe '#fizzbuzz' do
+    it('should return fizz if n divisible by 3') do
+      expect(@solver.fizzbuzz(3)).to eq 'fizz'
     end
+    it('should return buzz if n divisible by 5') do
+      expect(@solver.fizzbuzz(5)).to eq 'buzz'
+    end
+    it('should return fizz if n divisible by 3') do
+      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+    it('should return n as a string if n is not divisible by 3 or 5') do
+      expect(@solver.fizzbuzz(7)).to eq '7'
+    end
+  end
 end
