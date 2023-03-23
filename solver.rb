7b49a954917db47fca85097aff63566e16c3943a
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-23 12:12:46 +0300

    Create fizzbuzz function

--- solver.rb
+++ solver.rb
@@ -17,5 +17,9 @@ class Solver
     end
 
     def fizzbuzz(n)
+        return "fizzbuzz" if(n%3==0 && n%5==0)
+        return "fizz" if(n%3==0)
+        return "buzz" if(n%5==0)
+        return "#{n}"
     end
 end
